11fab45a7cd648b0775edb7438f521bae02e4045
盛辉 <hui.sheng@sunmi.com>
2025-07-04 17:50:03 +0800

    Support prometheus metrics

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,11 @@ fn main() {
     // 解析command
     let cli = cmd::Cli::parse();
     // 处理command
-    if let Some(v) = cli.command {
-        match v {
-            cmd::Command::New { name, axum, app } => cmd::project::run(name, axum, app),
-            cmd::Command::App { name, axum } => cmd::app::run(name, axum),
+    match cli.command {
+        Some(cmd::Command::New { name, axum, app }) => cmd::project::run(name, axum, app),
+        Some(cmd::Command::App { name, axum }) => cmd::app::run(name, axum),
+        _ => {
+            println!("欢迎使用noble-gase[Rust]脚手架");
         }
     }
 }
